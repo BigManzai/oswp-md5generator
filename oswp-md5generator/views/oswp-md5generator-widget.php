8864df8f848a0a981dc73a4187d428a3898dcf2c
BigManzai <39168019+BigManzai@users.noreply.github.com>
2019-02-03 19:25:37 +0100

    upgrade1

--- oswp-md5generator/views/oswp-md5generator-widget.php
+++ oswp-md5generator/views/oswp-md5generator-widget.php
@@ -2,28 +2,17 @@
 <!-- This file is used to markup the public-facing widget. -->
 
 
-
-
-<body>
-
-
-    
-    
-    <?php
- echo "<h4>MD5-Hash-Generator</h4>";
-	echo '<form action="'.$PHP_SELF.'" method="post">
- 
-<h4>Passworteingabe:</h4>
-	<input name="passwort">
- 
-	<input type="submit" name="md5" value="Ermittle MD5-Hash">
- 
-	</form>';
-echo "<h4>MD5-Hash Ausgabe:</h4>";
-echo "<h6>". md5($_POST["passwort"]) ."</h5>";
- 
-?>
- 
-</body>
-</html>
 <?php
+$PHP_SELF = &$_SERVER['PHP_SELF'];
+
+echo "<h4>MD5-Hash-Generator</h4>";
+
+echo '<form action="' . $PHP_SELF . '" method="post">
+<h4>Passworteingabe:</h4>
+<input name = "passwort">
+<input type="submit" name="md5" value="Ermittle MD5-Hash">
+</form>';
+
+echo "<h4>MD5-Hash Ausgabe:</h4>";
+
+echo "<h6>". md5($_POST["passwort"]) ."</h6>"; 
